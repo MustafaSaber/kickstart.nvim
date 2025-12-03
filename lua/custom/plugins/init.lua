c39fc633117dc749a340b2d433ed326898a67719
Mustafa Abdelrahman <mustafa.abdelrahman@zalando.de>
2025-12-03 14:44:37 +0100

    Add AI for suggestions

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,53 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+  'yetone/avante.nvim',
+  event = 'VeryLazy',
+  lazy = false,
+  version = false, -- Set this to "*" to always pull the latest release version, or set it to false to update to the latest code changes.
+  opts = {
+    provider = 'openai',
+    auto_suggestions_provider = 'openai',
+    providers = {
+      openai = {
+        endpoint = 'https://zllm.data.zalan.do/v1',
+        model = 'bedrock/anthropic.claude-sonnet-4-20250514-v1:0',
+        api_key_name = 'cmd:ztoken',
+        extra_request_body = {
+          temperature = 0.3,
+          max_tokens = 8000,
+        },
+      },
+    },
+  },
+  build = 'make',
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    'MunifTanjim/nui.nvim',
+    --- The below dependencies are optional,
+    {
+      -- support for image pasting
+      'HakonHarnes/img-clip.nvim',
+      event = 'VeryLazy',
+      opts = {
+        -- recommended settings
+        default = {
+          embed_image_as_base64 = false,
+          prompt_for_file_name = false,
+          drag_and_drop = {
+            insert_mode = true,
+          },
+        },
+      },
+    },
+    {
+      -- Make sure to set this up properly if you have lazy=true
+      'MeanderingProgrammer/render-markdown.nvim',
+      opts = {
+        file_types = { 'markdown', 'Avante' },
+      },
+      ft = { 'markdown', 'Avante' },
+    },
+  },
+}
